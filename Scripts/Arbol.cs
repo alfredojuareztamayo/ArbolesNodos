91265e2b12e39b10a3237ad07e28fee40da71950
Juan Alfredo <38137911+alfredojuareztamayo@users.noreply.github.com>
2023-06-09 13:42:13 -0600

    Add files via upload

--- Scripts/Arbol.cs
+++ Scripts/Arbol.cs
@@ -84,30 +84,32 @@ Temp = 11
         Debug.Log("No existe dicho valor en el arbol");
         return null;
     }
-
-    //private void PrintTree(Queue q1)
-    //{
-    //    string treeText = " ";
-    //    NodoArbol tree = adam;
-    //   // NodoArbol temp = null;
-    //    while(tree != null)
-    //    {
-    //        if(tree.left.dato < tree.dato)
-    //        {
-    //            tree = tree.left;
-    //        }
-    //        else
-    //        {
-    //            tree = tree.right;
-    //        }
-    //        /*if(tree.right.dato < tree.dato)
-    //        {
-    //            tree = tree.right;
-    //        }*/
-            
-    //    }
-       
-    //}
+    public NodoArbol FindParent(int num)
+    {
+        // NodoArbol temp = null;
+        NodoArbol actual = adam;
+        while (actual != null)
+        {
+            if (num == actual.left.dato || num == actual.right.dato)
+            {
+                Debug.Log(actual.dato);
+                return actual;
+            }
+                if (num > actual.left.dato)
+                {
+                
+                    actual = actual.right;
+                }
+                else
+                {
+               
+                    actual = actual.left;
+                }
+        }
+        Debug.Log("No existe dicho valor en el arbol");
+        return null;
+    }
+    
     private void PrintOne(NodoArbol tree, StringBuilder T)
     {
         
@@ -184,5 +186,78 @@ Temp = 11
         StringBuilder builder = new StringBuilder("Los valores del arbol de mayor a menor: ");
         PrintFour(adam, builder);
         Debug.Log(builder.ToString());
+    }
+
+    public void DeleteArbol(int num)
+    {
+        NodoArbol parent = FindParent(num);
+        NodoArbol child = Find(num);
+
+        if(child.left == null && child.right == null)
+        {
+            if (parent.right == child)
+            {
+                parent.SetRight(null);
+            }
+            else
+            {
+                parent.SetLeft(null);
+            }
+        }
+        if (child.left == null || child.right == null)
+        {
+            if(child.left == null)
+            {
+                parent.SetRight(child.right);
+            }
+            else
+            {
+                parent.SetLeft(child.right);
+            }
+        }
+        if (child.left != null && child.right != null)
+        {
+            NodoArbol temp = child;
+            if (parent.right == child)
+            {
+                while (temp.left != null)
+                {
+                    if (temp.right.left != null && temp.right.right != null)
+                    {
+                        temp = temp.right;
+                        continue;
+                    }
+                    if (temp.right == null)
+                    {
+                        temp = temp.left;
+                        continue;
+                    }
+                    temp = temp.left;
+                }
+            temp.SetLeft(child.left);
+            temp.SetRight(child.right);
+            parent.SetRight(temp);
+            }
+            if (parent.left == child)
+            {
+                while (temp.right != null)
+                {
+                    if (temp.right.left != null && temp.right.right != null)
+                    {
+                        temp = temp.right;
+                        continue;
+                    }
+                    if (temp.right == null)
+                    {
+                        temp = temp.left;
+                        continue;
+                    }
+                    temp = temp.left;
+                }
+                temp.SetLeft(child.left);
+                temp.SetRight(child.right);
+                parent.SetRight(temp);
+            }
+        }
     }
 }
